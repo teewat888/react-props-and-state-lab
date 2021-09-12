1e79fdc24de95bd0f93eb865ffb6ae94e7eba00b
teerawat prarom <teewat@yahoo.com>
2021-09-12 15:00:22 +1000

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
+import fetchMock from '../fetch-setup'
 
 class App extends React.Component {
   constructor() {
@@ -15,6 +16,55 @@ class App extends React.Component {
     }
   }
 
+  handleChangeType = (type) => {
+    console.log('type ',type);
+    this.setState({
+      filters: {
+        type: type
+      } 
+    })
+  }
+
+  handleFindPetsClick = () => {
+    if (this.state.filters.type === 'all') {
+      fetch('/api/pets').then(resp => resp.json()).then(data => {
+        this.setState({
+          pets: data
+        },() => console.log(this.state))
+      })
+      
+    } else {
+      fetch('/api/pets?type='+this.state.filters.type).then(resp => resp.json()).then(data => {
+        this.setState({
+          pets: data
+        },() => console.log(this.state))
+      })
+    }
+  }
+
+
+  handleAdoptPet = (id) => {
+     const newPets = this.state.pets.map((pet) => {
+       if(pet.id === id) {
+         return {
+          age: pet.age,
+          gender: pet.gender,
+          id: pet.id,
+          isAdopted: true,
+          name: pet.name,
+          type: pet.type,
+          weight: pet.weight   
+         }
+       } else return (pet);
+     })
+     //const newPets = this.state.pets.splice(petIdx,1,newPet);
+     console.log('new pet  ',newPets);
+    this.setState({
+      pets: newPets
+    },() => {console.log('new state ',this.state);})
+     //to set this.state to isAdopted = true
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +74,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleChangeType} onFindPetsClick={this.handleFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.handleAdoptPet} />
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,15 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+  makePetList = () => {
+    return this.props.pets.map(pet => <Pet 
+      pet={pet}
+      onAdoptPet={this.props.onAdoptPet}
+      key={pet.id}
+      />)
+  }
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards">{this.makePetList()}</div>
   }
 }
 
